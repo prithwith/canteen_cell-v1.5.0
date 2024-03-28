d934eee6e50e35c937e8a6ae7d2d07bfd8e859ef
PRITHWITH SAHOO <162318055+prithwith@users.noreply.github.com>
2024-03-28 16:21:26 +0530

    Update url1.dart

--- lib/utils/url1.dart
+++ lib/utils/url1.dart
@@ -1,11 +1,3 @@
-// class MyUrl {
-//   static String mainurl = "cclms.in";
-//   static String suburl = "CCLMS_Canteen/canteen_cell_api/";
-//   static String imageurl = "profilepic/";
-//   static String adminimageurl = "adminprofilepic/";
-//   static String fullurl = "http://cclms.in/CCLMS_Canteen/canteen_cell_api/";
-// }
-
 class MyUrl {
   static String mainurl = "www.cclmsit.com";
   static String suburl = "bca21app/prithwith/canteen_cell_api/";
@@ -14,12 +6,3 @@ class MyUrl {
   static String fullurl =
       "http://www.cclmsit.com/bca21app/prithwith/canteen_cell_api/";
 }
-
-// class MyUrl {
-//   static String mainurl = "canteencell.000webhostapp.com";
-//   static String suburl = "canteencell/public_html/canteen_cell_api/";
-//   static String imageurl = "profilepic/";
-//   static String adminimageurl = "adminprofilepic/";
-//   static String fullurl =
-//       "https://canteencell.000webhostapp.com/canteencell/public_html/canteen_cell_api/";
-// }
